f5cd7856058060ed196c0d9a838b89ddd41fb1d1
harharkaur <90332134+harharkaur@users.noreply.github.com>
2023-05-09 19:26:53 -0400

    Create Grad.java

---
+++ Grad.java
@@ -0,0 +1,33 @@
+package inheritance;
+
+
+	public class Grad extends Student{
+
+	    String major = "";
+
+	    public Grad(String name, long studentID, String major) {
+	        super(name, studentID);
+	        personType = 3;
+	        this.major = major;
+	    }
+
+	    public String getMajor() {
+	        return major;
+	    }
+
+	    public void setMajor(String major) {
+	        this.major = major;
+	    }
+
+	    public void print() {
+	        super.print();
+	        System.out.println("Major: " + major);
+	    }
+
+	    public boolean equals(Grad other) {
+	        return this.major.equals(other.getMajor());
+	    }
+
+	}
+
+
